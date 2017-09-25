3060270d568f940a08e2e1107a67c654b1fcf912
AlexisSConstantakos <LEXC023@GMAIL.COM>
2017-09-25 09:36:16 -0700

    data binding example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,13 @@ import {NgForm} from '@angular/forms'
 export class AppComponent {
   title = 'app';
 
-  onSubmit(data: NgForm){
-    console.log(data.value) //.value represents the fields on the form
+
+  model: object = {
+    first_name: "Bob",
+    last_name: "Smith"
+  }
+
+  onSubmit(){
+    console.log(this.model) //.value represents the fields on the form
   }
 }
